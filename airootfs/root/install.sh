cefc66e54d67d5dccb7de174a155b47be737e1c6
USERNAME <EMAIL>
2023-04-12 23:18:05 +0700

    corrected install and post install

--- airootfs/root/install.sh
+++ airootfs/root/install.sh
@@ -1,14 +1,9 @@
 echo 'Enter your username:'
 read USERNAME
 sed -i "s/<USERNAME>/$USERNAME/g" creds.json
-sed -i "s/<USERNAME>/$USERNAME/g" gitconfig
 
 echo 'Enter your password:'
-read PASSOWRD
-sed -i "s/<PASSOWRD>/$PASSWORD/g" creds.json
-
-echo 'Enter your email:'
-read EMAIL
-sed -i "s/<EMAIL>/$EMAIL/g" gitconfig.json
+read PASSWORD
+sed -i "s/<PASSWORD>/$PASSWORD/g" creds.json
 
 archinstall --silent --disk_layouts disk.json --config config.json --creds creds.json
--- airootfs/root/post_install.sh
+++ airootfs/root/post_install.sh
@@ -5,7 +5,7 @@ cp /etc/os-release /mnt/archinstall/etc/os-release
 cp /etc/pacman.conf /mnt/archinstall/etc/pacman.conf
 sed -i 's/timeout 3/timeout 0/g' /mnt/archinstall/boot/loader/loader.conf
 
-cp -R /root /mnt/archinstall/home/$USERNAME
+cp -R /root/* /mnt/archinstall/home/$USERNAME
 
 mkdir -p /mnt/archinstall/usr/local/share/backgrounds
 cp /usr/local/share/backgrounds/wallpaper.jpg /mnt/archinstall/usr/local/share/backgrounds/wallpaper.jpg
@@ -14,4 +14,5 @@ cp /usr/local/share/backgrounds/fleu-linux.png /mnt/archinstall/usr/local/share/
 chmod a+rwx -R /mnt/archinstall/home/$USERNAME
 
 arch-chroot -u $USERNAME /mnt/archinstall dconf load / < /home/$USERNAME/dconf.ini
-arch-chroot /mnt/archinstall pacman -R --noconfirm epiphany gnome-music
+arch-chroot /mnt/archinstall pacman -R --noconfirm epiphany gnome-music gnome-console
+arch-chroot /mnt/archinstall zsh 'chsh -s $(which zsh)'
