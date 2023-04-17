d93d0a8643baab5b8664a2107131c4d4226ac496
dancheg97 <dancheg97@fmnx.su>
2023-04-17 16:53:34 +0700

    added bluetooth stuff to notebooks

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -2,3 +2,4 @@
 xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmnx-linux.png 
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
 sed -i 's|1|2|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
+systemctl start bluetooth.service
