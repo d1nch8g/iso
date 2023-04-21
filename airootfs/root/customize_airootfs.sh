ecf3db20ba310e554135b48de1c36457996e18d8
dancheg97 <dancheg97@fmnx.su>
2023-04-21 11:40:30 +0700

    customized archinstall not to install trash packages

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmnx-linux.png 
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
-sed -i 's|1|2|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
+sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon", "gnome-disk-utility", "gnome-keyring", "gnome-logs", "gnome-system-monitor", "gnome-shell-extensions", "gnome-tweaks", "gnome-characters", "gnome-font-viewer", "gnome-shell-extension-dash-to-dock", "gnome-shell-extension-gtile",|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
 systemctl start bluetooth.service
