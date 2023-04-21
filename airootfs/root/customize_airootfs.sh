fb3a26fadf60d0e1e134d1310468f1069a2bb137
dancheg97 <dancheg97@fmnx.su>
2023-04-21 12:26:03 +0700

    customized inner for gnome packages

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -2,4 +2,3 @@
 xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmnx-linux.png 
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
 sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon", "gnome-disk-utility", "gnome-keyring", "gnome-logs", "gnome-system-monitor", "gnome-shell-extensions", "gnome-tweaks", "gnome-characters", "gnome-font-viewer", "gnome-shell-extension-dash-to-dock", "gnome-shell-extension-gtile",|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
-systemctl start bluetooth.service
