afb05d3b6db1d328b363783b9eb193592093d184
dancheg97 <dancheg97@fmnx.su>
2023-04-25 16:44:12 +0700

    corrected additional package list for gnome environment

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmnx-linux.png 
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
-sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon", "gnome-disk-utility", "gnome-keyring", "gnome-logs", "gnome-system-monitor", "gnome-shell-extensions", "gnome-tweaks", "gnome-characters", "gnome-font-viewer",|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
+sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon",|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
