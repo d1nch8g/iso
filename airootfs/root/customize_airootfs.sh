7458813f8fefd20715df17f5dfe15bb9f63a59c9
dancheg97 <dancheg97@fmnx.su>
2023-05-05 19:33:58 +0700

    fixed customize airootfs to support python latest version

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmnx-linux.png 
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
-sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon",|g' /usr/lib/python3.10/site-packages/archinstall/profiles/gnome.py
+sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon",|g' /usr/lib/python3.11/site-packages/archinstall/profiles/gnome.py
 sed -i 's|Arch Linux|FMNX Linux|g' /usr/lib/python3.10/site-packages/archinstall/lib/installer.py
