ae846c47b1ea2ebc767306504401671af7c1906d
dancheg97 <dancheg97@fmnx.su>
2023-06-07 02:19:06 +0700

    moved additional packages to ainst yml, so that they are installed with updated pacman configuration file

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -3,4 +3,4 @@ xdg-icon-resource install --novendor --size 512 /usr/local/share/backgrounds/fmn
 sed -i 's|/usr/bin/chromium|/usr/bin/chromium --no-sandbox|g' /usr/share/applications/chromium.desktop
 sed -i 's|"gnome",|"gnome-shell", "gnome-session", "gnome-terminal", "gnome-control-center", "gnome-settings-daemon",|g' /usr/lib/python3.11/site-packages/archinstall/profiles/gnome.py
 # replace that line when archinstall pacman-init service is fixed
-sed -i 's|pacman-init|reflector|g' /usr/lib/python3.11/site-packages/archinstall/lib/installer.py
+sed -i 's|pacman-init|reflector|g' /usr/lib/python3.11/site-packages/archinstall/examples/guided.py
